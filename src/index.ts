85bfa2f812e70b2716bcc9f7c17741ba9cc4463d
Jameslinimk <jameskimimk@gmail.com>
2023-03-19 10:39:14 -0400

    Stf

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import chalk from "chalk"
 import ChessImageGenerator from "chess-image-generator"
 import { Chess } from "chess.js"
+import { Presets, SingleBar } from "cli-progress"
 import ffmpeg from "fluent-ffmpeg"
 import { existsSync } from "fs"
-import { mkdir, readFile, readdir } from "fs/promises"
+import { mkdir, readFile, readdir, rmdir } from "fs/promises"
 import { dirname, join } from "path"
 import { fileURLToPath, pathToFileURL } from "url"
 import { desiredTime, pgnFile, videoOutput } from "./config.js"
@@ -30,52 +31,64 @@ const main = async () => {
     } catch (err) {
         error("Invalid PGN! " + `${err}`.replace("Error: Invalid FEN: ", ""))
     }
+    const rawHistory = chess.history({ verbose: true })
 
     const images: string[] = []
     const tempPath = join(__dirname, "../temp")
-    if (!existsSync(tempPath)) {
-        await mkdir(tempPath)
+    const files = existsSync(tempPath) ? await readdir(tempPath) : []
+
+    if (files.length === 0 || files.length !== rawHistory.length) {
+        if (existsSync(tempPath)) await rmdir(tempPath, { recursive: true })
+        if (!existsSync(tempPath)) await mkdir(tempPath)
 
         const imageGenerator = new ChessImageGenerator()
-        const history = chess.history({ verbose: true }).map((move) => move.after)
+        const history = rawHistory.map((move) => move.after)
+
+        const bar = new SingleBar({}, Presets.shades_grey)
+        console.log(chalk.green("Generating images..."))
+        bar.start(history.length, 0)
 
         for await (const [i, fen] of history.entries()) {
-            if (i !== 0 && i % 1000 === 0) {
-                console.log(chalk.greenBright("Waiting for 2 seconds to prevent corruption..."))
-                await wait(2000)
-            }
+            if (i !== 0 && i % 1000 === 0) await wait(2000)
 
             imageGenerator.loadFEN(fen)
             const path = join(__dirname, `../temp/temp_${i}.png`)
             await imageGenerator.generatePNG(path)
             images.push(path)
-            console.log(chalk.green(`Generated image ${i + 1}/${history.length}`))
+            bar.update(i + 1)
         }
+
+        bar.stop()
     } else {
-        console.log(chalk.green("Found temp folder, skipping image generation"))
-        const files = await readdir(tempPath)
+        console.log(chalk.green("Found temp folder with the same amount of frames, skipping image generation"))
         files.forEach((file) => images.push(join(tempPath, file)))
     }
 
     const fps = Math.round(images.length / desiredTime)
 
     console.log()
+
+    const videoPath = join(__dirname, "../", videoOutput)
+    if (existsSync(videoPath)) console.log(chalk.yellow("Video already exists, overwriting..."))
+    else console.log(chalk.green("Generating video..."))
+
+    const bar = new SingleBar({}, Presets.shades_grey)
+    bar.start(images.length, 0)
+
     ffmpeg()
         .input("./temp/temp_%d.png")
         .inputOptions([`-framerate ${fps}`])
         .videoCodec("libx264")
         .outputOptions(["-pix_fmt yuv420p"])
-        .save(join(__dirname, "../", videoOutput))
-        .on("start", (commandLine) => {
-            console.log(chalk.greenBright(`FFmpeg started with command: ${chalk.gray(commandLine)}`))
-        })
+        .save(videoPath)
         .on("progress", (progress) => {
-            const percent = Math.round((progress.frames / images.length) * 100)
-            console.log(chalk.green(`Processing, ${percent}% done`))
+            bar.update(progress.frames)
         })
-        .on("error", console.error)
+        .on("error", error)
         .on("end", () => {
-            console.log(chalk.green("Video generated!"))
+            bar.stop()
+            console.log()
+            console.log(chalk.cyan("Video generated!"))
         })
 }
 
